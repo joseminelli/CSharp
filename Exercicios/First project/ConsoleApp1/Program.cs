38e91575adb8f91e14742eb109780bf88a69218a
joseminelli <zecarminelli@gmail.com>
2024-02-01 09:22:44 -0300

    first project

--- Exercicios/First project/ConsoleApp1/Program.cs
+++ Exercicios/First project/ConsoleApp1/Program.cs
@@ -8,9 +8,58 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+
+        static void Encerrar(string texto = null)
+        {
+            try
+            {
+                if (texto != null)
+                {
+                    Console.WriteLine(texto);
+                }
+                else
+                {
+                    Console.WriteLine("Aperte ENTER para sair");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+
+            Console.ReadKey(); 
+        }
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("=== Menu === \n 1 - Cadastrar \n 2 - Listar \n 3 - Sair");
+            char opcao;
 
+            do
+            {
+                Console.Write("\n Digite a opção desejada: ");
+                opcao = Console.ReadKey().KeyChar;
+                Console.WriteLine("\n");
+                switch (opcao)
+                {
+                    case '1':
+                        Console.WriteLine("Cadastrar");
+                        break;
+                    case '2':
+                        Console.WriteLine("Listar");
+                        break;
+                    case '3':
+                        Console.WriteLine("Sair");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+            } while (opcao != '3');
+
+            Encerrar();
         }
+
     }
 }
