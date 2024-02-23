65f856c13305740b80796b7e8259d1c6cb52ee3a
José Minelli <zecarmineli@gmail.com>
2024-02-22 21:44:02 -0300

    Cores no terminal + boas vindas

--- Projetos/PrimeiroProjeto/Arquivos/Program1.cs
+++ Projetos/PrimeiroProjeto/Arquivos/Program1.cs
@@ -5,10 +5,27 @@ using System.Threading;
 
 namespace PrimeiroProjeto
 {
-    
+
     internal class Program1
     {
-        
+        public static void ColorText(string texto, string cor)
+        {
+            // tenta converter a string 'cor' pra um valor do enum ConsoleColor
+            // o parâmetro 'true' indica que a conversão é insensível a maiúsculas/minúsculas
+            // a variável 'consoleColor' vai ser usada para armazenar o resultado da conversão com o 'out' definindo a saída do resultado.
+            if (!Enum.TryParse<ConsoleColor>(cor, true, out ConsoleColor consoleColor))
+            {
+                // lança uma exceção indicando que a cor é inválida se a conversão falhar
+                throw new ArgumentException("Cor inválida", nameof(cor));
+            }
+
+            Console.ForegroundColor = consoleColor;
+
+            Console.WriteLine(texto);
+
+            // volta a cor original do console
+            Console.ResetColor();
+        }
 
         static void Encerrar(string texto = null)
         {
@@ -33,40 +50,75 @@ namespace PrimeiroProjeto
 
         static void Delay(int tempo)
         {
-            Thread.Sleep(tempo); 
+            Thread.Sleep(tempo);
         }
 
+        static void BoasVindas()
+        {
+            ColorText("Bem vindo ao sistema", "green");
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadKey();
+            Console.Clear();
+        }
+       
         static void Main()
         {
+            BoasVindas();
+
             Console.Write("Digite seu nome: ");
             String nome = Console.ReadLine();
 
-            Console.Clear();
+            int idade;
+            try
+            {
+                do
+                {
+                    Console.Clear();
 
-            Console.Write("Digite sua idade: ");    
-            int idade = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Digite sua idade: ");
+                    idade = Convert.ToInt32(Console.ReadLine());
+                } while (idade <= 0 || idade > 99);
+            }
+            catch (Exception ex)
+            {
+                ColorText("Erro: " + ex.Message, "red");
+                Encerrar();
+                return;
+            }
+            
 
             Console.Clear();
 
             Console.Write("Digite o nome da sua Rua: ");
             String rua = Console.ReadLine();
 
+            int numeroDaCasa;
             Console.Clear();
-
-            Console.Write("Digite o número da sua casa: ");
-            int numeroDaCasa = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.Write("Digite o número da sua casa: ");
+                numeroDaCasa = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                ColorText("Erro: " + ex.Message, "red");
+                Encerrar();
+                return;
+            }
 
             Console.Clear();
 
             string genero = "";
             do
             {
-                Console.Write("Informe seu gênero \n [M] - Masculino \n [F] - Feminino\n");
+                Console.Write("Informe seu gênero");
+                ColorText("\n [M] - Masculino", "blue");
+                ColorText("\n [F] - Feminino", "Magenta");
                 genero = Console.ReadKey().KeyChar.ToString();
                 Console.Clear();
             } while (genero != "m" && genero != "f" && genero != "M" && genero != "F" );
 
-            Console.WriteLine("Aguarde um momento...");
+            ColorText("Aguarde um momento...", "yellow");
             Delay(2000);
             Console.Clear();
 
